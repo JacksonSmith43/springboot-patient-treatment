fd947f43995fd1ccbdf71d1a1444b420815ebd25
JacksonSmith43 <jessica444@gmx.at>
2025-04-17 15:12:59 +0200

    Added doctors and patients.

---
+++ src/main/java/com/patient/patient_treatment/DoctorRepository.java
@@ -0,0 +1,8 @@
+package com.patient.patient_treatment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+
+
+}
--- src/main/java/com/patient/patient_treatment/PatientTreatmentApplication.java
+++ src/main/java/com/patient/patient_treatment/PatientTreatmentApplication.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Bean;
 
 import java.util.List;
 
-
 @SpringBootApplication
 public class PatientTreatmentApplication {
 
@@ -16,12 +15,33 @@ public class PatientTreatmentApplication {
     }
 
     @Bean
-    CommandLineRunner commandLineRunner(PatientRepository patientRepository) {
+    CommandLineRunner patientCommandLineRunner(PatientRepository patientRepository) {
         return args -> {
             Patient birgit = new Patient("Birgit", "Cadogan", "22.02.2023");
             Patient jackson = new Patient("Jackson", "Smith", "23.02.1998");
+            Patient janet = new Patient("Janet", "Jackson", "24.03.1957");
+            Patient michael = new Patient("Michael", "Yoshi", "04.09.2023");
+            Patient donatello = new Patient("Donatello", "O´Neil", "13.09.1988");
+            Patient mickey = new Patient("Miekey", "Patrenko", "14.12.1997");
+            Patient raphael = new Patient("Raphael", "Fedori", "02.01.1907");
 
-            patientRepository.saveAll(List.of(birgit, jackson));
+            patientRepository.saveAll(List.of(birgit, jackson, janet, michael, donatello, mickey, raphael));
+        };
+    }
+
+
+    @Bean
+    CommandLineRunner doctorCommandLineRunner(DoctorRepository doctorRepository) {
+        return args -> {
+            Doctor theDoctor = new Doctor("The Doctor", "Weirdo Awareness");
+            Doctor hibbert = new Doctor("Hibbert", "Neurology");
+            Doctor murphy = new Doctor("Murphy", "Endocrinologist");
+            Doctor naylor = new Doctor("Naylor", "Cardiology");
+            Doctor levy = new Doctor("Levy", "Pulmonologists");
+            Doctor piper = new Doctor("Piper", "Oncology");
+            Doctor ngozi = new Doctor("Ngozi", "Psychiatry");
+
+            doctorRepository.saveAll(List.of(theDoctor, hibbert, murphy, naylor, levy, piper, ngozi));
         };
     }
 
